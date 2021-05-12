2c1ef9139f749ff01d599be1702c8f6bc21b235f
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-12 09:59:23 +0300

    made the quickstart code better

--- index.js
+++ index.js
@@ -1,141 +1,79 @@
-var fs = require('fs');
-var readline = require('readline');
-var {google} = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
+const fs = require('fs')
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/youtube-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+const readline = require('readline')
+const { google } = require('googleapis')
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
 const TOKEN_PATH = 'token.json'
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the YouTube API.
-  authorize(JSON.parse(content), getChatMessages);
-});
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err)
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), getChannel)
+})
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+const authorize = (credentials, callback) => {
+    const { client_secret, client_id, redirect_uris } = credentials.installed
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback)
-    oAuth2Client.setCredentials(JSON.parse(token))
-    callback(oAuth2Client)
-  })
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
- const getNewToken = (oAuth2Client, callback) => {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  })
-  console.log('Authorize this app by visiting this url:', authUrl)
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err)
-      oAuth2Client.setCredentials(token)
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err)
-        console.log('Token stored to', TOKEN_PATH)
-      })
-      callback(oAuth2Client)
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback)
+        oAuth2Client.setCredentials(JSON.parse(token))
+        callback(oAuth2Client)
     })
-  })
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    if (err) throw err;
-    console.log('Token stored to ' + TOKEN_PATH);
-  });
+const getNewToken = (oAuth2Client, callback) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    })
+    console.log('Authorize this app by visiting this url:', authUrl)
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close()
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error while trying to retrieve access token', err)
+            oAuth2Client.setCredentials(token)
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err)
+                console.log('Token stored to', TOKEN_PATH)
+            })
+            callback(oAuth2Client)
+        })
+    })
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function getChannel(auth) {
-  var service = google.youtube('v3');
-  service.channels.list({
-    auth: auth,
-    part: 'snippet,contentDetails,statistics',
-    id: 'UCbZRGXMhWPva6OZydKs70ng'
-    // forUsername: 'GoogleDevelopers'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var channels = response.data.items;
-    if (channels.length == 0) {
-      console.log('No channel found.');
-    } else {
-      console.log(`This channel's ID is ${channels[0].id}. 
-      Its title is ${channels[0].snippet.title}, 
-      it has ${channels[0].statistics.viewCount} views and 
-      it has ${channels[0].statistics.subscriberCount} subscribers.`)
-    }
-  });
-}
+const getChannel = (auth) => {
+    const service = google.youtube('v3')
 
-function getChatMessages(auth) {
-  var service = google.youtube('v3');
-  service.liveChatMessages.list({
-    auth: auth,
-    liveChatId: "FsYH7vTzUqI",
-    part: "snippet,authorDetails"
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
+    const request = {
+        auth: auth,
+        part: 'snippet,contentDetails,statistics',
+        id: 'UCbZRGXMhWPva6OZydKs70ng',
     }
-    var liveChatMessages = response.data.items;
-    if (liveChatMessages.length == 0) {
-      console.log('No channel found.');
-    } else {
-      console.log(liveChatMessages)
-      // console.log(`Livestream id: ${liveChatMessages[0].liveChatId}. 
-      // Chat ${liveChatMessages[0].snippet}`)
-    }
-  });
+
+    service.channels.list(request, (err, response) => {
+        if (err) return console.log('The API returned an error: ' + err)
+        const channels = response.data.items
+        if (channels.length == 0) {
+            console.log('No channel found.')
+        } else {
+            console.log(`This channel's ID is ${channels[0].id}. 
+            Its title is ${channels[0].snippet.title}, 
+            it has ${channels[0].statistics.viewCount} views and 
+            it has ${channels[0].statistics.subscriberCount} subscribers.`)
+        }
+    })
 }
